54ec71cbc648f1b2908297116553092c90f5bb07
JUAN ANTONIO DURÁN <142181516+juanantoniocimada@users.noreply.github.com>
2026-06-30 11:02:39 -0100

    feat: nestjs conexion

--- nostaxi/src/app/app.ts
+++ nostaxi/src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NestJSService } from './services/nestjs.service';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HttpClientModule
+  ],
+  providers: [NestJSService],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('nostaxi');
+export class App implements OnInit {
+  protected readonly title = signal('NOSTAXI.com');
+
+  public vehicles: any = [];
+
+  public nestjsService = inject(NestJSService);
+
+  ngOnInit() {
+    this.getVehicles();
+  }
+
+  getVehicles() {
+    this.nestjsService.getVehicles().subscribe({
+      next: (data) => {
+        console.log('Vehicles:', data);
+        this.vehicles = data['data'];
+        console.log(this.vehicles);
+        
+      },
+      error: (error) => {
+        console.error('Error fetching vehicles:', error);
+      }
+    });
+  }
 }
---
+++ nostaxi/src/app/services/nestjs.service.ts
@@ -0,0 +1,22 @@
+
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NestJSService {
+
+  private _apiUrl = window.location.hostname === 'localhost'
+    ? environment.nestJsLocal || environment.nestJsLocal
+    : environment.nestJsLocal;
+  private _http = inject(HttpClient);
+
+  public getVehicles(): Observable<any> {
+    const url = `${this._apiUrl}/vehicles`;
+    return this._http.get(url)
+  }
+}
